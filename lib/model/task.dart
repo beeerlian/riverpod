d4f0f9124b18716378ab920e74960714b3c56e3f
beeerlian <naufal.berlian.99@gmail.com>
2021-09-12 17:50:19 +0700

    delete unused comment

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -3,18 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todolist_app/provider/check_box_provider.dart';
 
-// class Task extends ChangeNotifier {
-//   Task({required this.title, required this.desc}) {
-//     checkBoxValueProvider =
-//         ChangeNotifierProvider((ref) => this.checkBoxNotifier);
-//   }
-
-//   String title;
-//   String desc;
-//   CheckBoxNotifier checkBoxNotifier = CheckBoxNotifier();
-//   late final checkBoxValueProvider;
-// }
-
 class Task extends ChangeNotifier {
   Task({required this.title, required this.desc}) {
     checkBoxValueProvider =
